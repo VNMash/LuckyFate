5143f1c5a99db505197583245f4d88b3d9136326
V8 <vladthomasnicola@gmail.com>
2025-07-10 00:22:45 +0300

    Updated Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,7 +4,7 @@ const Logo = () => {
   return (
     <div className="fixed top-4 left-4 z-[60]">
       <img 
-        src="/1315e586-9def-4101-81b6-1d42458f649e.jpg" 
+        src="/50bb1bac-7ed1-40bf-9f2a-5e80e3762c45.jpg" 
         alt="LuckyFate Logo"
         className="h-24 w-auto object-contain opacity-100 drop-shadow-2xl"
         style={{
