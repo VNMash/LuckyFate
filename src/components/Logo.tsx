fdcef0372a119e70cd7a3ac1ba2c9c26c65f43e0
V8 <vladthomasnicola@gmail.com>
2025-07-10 21:32:53 +0300

    Updated Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Logo = () => {
   return (
-    <div className="fixed top-2  left-2  z-[60]">
+    <div className="fixed top-2  left-4  z-[60]">
       <img 
         src="/Знімок екрана 2025-07-10 151119.png" 
         alt="LuckyFate Logo"
-        className="h-32 w-auto object-contain  opacity-100 drop-shadow-2xl"
+        className="h-34 w-auto object-contain  opacity-100 drop-shadow-2xl"
         style={{
           filter: 'drop-shadow(0 8px 24px rgba(0, 0, 0, 0.5))'
         }}
